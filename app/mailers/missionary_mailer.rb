9382c245c4f69ca552a0672987056498945cb8c5
Scott Wolff <scott.wolff@tuscan.ca>
2010-06-30 00:16:48 -0600

    stub out missionary mailer

--- app/mailers/missionary_mailer.rb
+++ app/mailers/missionary_mailer.rb
@@ -1,22 +1,12 @@
 class MissionaryMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "scott.wolff@tuscan.ca"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.actionmailer.missionary_mailer.application_sent_notice.subject
-  #
-  def application_sent_notice
-    @greeting = "Hi"
+  def application_sent_notice(missionary)
+    @missionary = missionary
 
-    mail :to => "to@example.org"
+    mail(:to => missionary.email, :subject => "Some imortant information regarding your mission application.")
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.actionmailer.missionary_mailer.call_receieved_notice.subject
-  #
   def call_receieved_notice
     @greeting = "Hi"
 
